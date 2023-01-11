0622635796c22fe92d7862a5251218456c35d5fb
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-01-11 15:37:01 -0500

    react router dom

--- src/App.js
+++ src/App.js
@@ -7,28 +7,25 @@ event (used to grab information about input)
 */
 
 import "./App.css";
-import Axios from "axios";
-import { useEffect, useState } from "react";
+import {BrowserRouter as Router, Routes, Route }from 'react-router-dom'
+import { Home } from "./pages/Home";
+import { Menu } from "./pages/Menu";
+import { Contact } from "./pages/Contact";
+import { Error } from "./pages/Error";
+import  {Navbar } from "./pages/Navbar";
 
 function App() {
-  const [generatedExcuse, setGeneratedExcuse] = useState("");
-
-  const fetchExcuse = (excuse) => {
-    Axios.get(`https://excuser.herokuapp.com/v1/excuse/${excuse}/`).then(
-      (res) => {
-        setGeneratedExcuse(res.data[0].excuse);
-      }
-    );
-  };
-
   return (
     <div className="App">
-      <h1> Generate An Excuse </h1>
-      <button onClick={() => fetchExcuse("party")}> Party</button>
-      <button onClick={() => fetchExcuse("family")}> Family</button>
-      <button onClick={() => fetchExcuse("office")}> Office </button>
-
-      <p> {generatedExcuse} </p>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/menu" element={<Menu/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="*" element={<Error/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
