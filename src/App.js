17dd92608ba18c1d7e858e66ff627db2071520c0
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-01-12 22:30:40 -0500

    react hook form and yup, making forms in react

--- src/App.js
+++ src/App.js
@@ -7,37 +7,12 @@ event (used to grab information about input)
 */
 
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Home } from "./pages/Home";
-import { Profile } from "./pages/Profile";
-import { Contact } from "./pages/Contact";
-import { Error } from "./pages/Error";
-import { Navbar } from "./pages/Navbar";
-import { useState, createContext } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Form } from "./components/Form";
 
-// in this file, App is the highest-level component
 function App() {
-  const client = new QueryClient({defaultOptions: {
-    queries: {
-      // prevents refreshing of data when window is refocused
-      refetchOnWindowFocus: false
-    },
-  }});
-
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar/>
-          <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Error />} />
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+      <Form />
     </div>
   );
 }
