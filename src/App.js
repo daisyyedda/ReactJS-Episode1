2737fed5aa9d7c98dbfd9b000efcfb4ce000b5cd
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-01-12 22:59:25 -0500

    custom hooks with 2 examples

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 /*
 JSX (function that returns HTML tags)
+
 state (a variable that are subject to change)
+
 ... (an array composed of everything in the old array and the new thing after)
+
 && (followed by codes to execute should the condition be true)
+
 event (used to grab information about input)
+
+react query (library that allows you to make API calls)
+
+yup (library that allows you to validate data)
+
+custom hook (flexible function that can be used in multiple components)
+
 */
 
 import "./App.css";
-import { Form } from "./components/Form";
+import { Cat } from "./components/Cat";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true,
+      },
+    },
+  });
   return (
     <div className="App">
-      <Form />
+      <QueryClientProvider client={client}>
+        <Cat />
+      </QueryClientProvider>
     </div>
   );
 }
