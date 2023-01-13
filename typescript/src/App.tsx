674e36239578461ce96b6d5690328077844cc566
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2023-01-13 13:29:55 -0500

    installed redux toolkit

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import './App.css';
+import { Home } from "./pages/Home";
+import { Login } from "./pages/Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Contact } from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
